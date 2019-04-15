e55a4b4e0b3e6368eb599df7f00e775a9c4a0992
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-04-15 19:28:55 +0000

    initial merge of develop-branch into master

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/**
+ *  @file    main.cpp
+ *
+ *  @author  Tobias Anker
+ *  Contact: tobias.anker@kitsunemimi.moe
+ *
+ *  Apache License Version 2.0
+ */
+
+#include <iostream>
+#include <connection.h>
+
+int main()
+{
+    ToriiGateway::Connection* connection = new ToriiGateway::Connection();
+
+    int a = 0;
+    std::cin >> a;
+    return 0;
+}
