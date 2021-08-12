e983760dfc269d9c18f1e3a7b45b033444541afb
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-08-12 20:52:07 +0200

    update reponse types

--- src/http/http_session.cpp
+++ src/http/http_session.cpp
@@ -143,6 +143,39 @@ HttpRequestEvent::processRequest()
 }
 
 /**
+ * @brief HttpRequestEvent::getResponseType
+ * @param ext
+ * @return
+ */
+const std::string
+HttpRequestEvent::getResponseType(const std::string &ext)
+{
+    if(beast::iequals(ext, ".htm"))  { return "text/html"; }
+    if(beast::iequals(ext, ".html")) { return "text/html"; }
+    if(beast::iequals(ext, ".php"))  { return "text/html"; }
+    if(beast::iequals(ext, ".css"))  { return "text/css"; }
+    if(beast::iequals(ext, ".txt"))  { return "text/plain"; }
+    if(beast::iequals(ext, ".js"))   { return "application/javascript"; }
+    if(beast::iequals(ext, ".json")) { return "application/json"; }
+    if(beast::iequals(ext, ".xml"))  { return "application/xml"; }
+    if(beast::iequals(ext, ".swf"))  { return "application/x-shockwave-flash"; }
+    if(beast::iequals(ext, ".flv"))  { return "video/x-flv"; }
+    if(beast::iequals(ext, ".png"))  { return "image/png"; }
+    if(beast::iequals(ext, ".jpe"))  { return "image/jpeg"; }
+    if(beast::iequals(ext, ".jpeg")) { return "image/jpeg"; }
+    if(beast::iequals(ext, ".jpg"))  { return "image/jpeg"; }
+    if(beast::iequals(ext, ".gif"))  { return "image/gif"; }
+    if(beast::iequals(ext, ".bmp"))  { return "image/bmp"; }
+    if(beast::iequals(ext, ".ico"))  { return "image/vnd.microsoft.icon"; }
+    if(beast::iequals(ext, ".tiff")) { return "image/tiff"; }
+    if(beast::iequals(ext, ".tif"))  { return "image/tiff"; }
+    if(beast::iequals(ext, ".svg"))  { return "image/svg+xml"; }
+    if(beast::iequals(ext, ".svgz")) { return "image/svg+xml"; }
+
+    return "application/text";
+}
+
+/**
  * @brief send file, which was requested
  *
  * @return true, if successful, else false
@@ -165,17 +198,7 @@ HttpRequestEvent::sendFileFromLocalLocation(const std::string &dir,
     // set response-type based on file-type
     boost::filesystem::path pathObj(path);
     const std::string extension = pathObj.extension().string();
-    if(pathObj.extension().string() == ".html") {
-        m_response.set(http::field::content_type, "text/html");
-    } else if(pathObj.extension().string() == ".css") {
-        m_response.set(http::field::content_type, "text/css");
-    } else if(pathObj.extension().string() == ".js") {
-        m_response.set(http::field::content_type, "text/javascript");
-    } else if(pathObj.extension().string() == ".png") {
-        m_response.set(http::field::content_type, "image/png");
-    } else {
-        m_response.set(http::field::content_type, "text/plain");
-    }
+    m_response.set(http::field::content_type, getResponseType(extension));
 
     // read file and send content back
     std::string fileContent = "";
--- src/http/http_session.h
+++ src/http/http_session.h
@@ -79,6 +79,8 @@ private:
     bool processClientRequest(const std::string &path);
     bool processMonitoringRequest(const std::string &path);
     void processControlRequest(const std::string &path, const std::string &inputValues);
+
+    const std::string getResponseType(const std::string &ext);
 };
 
 #endif // HTTP_SESSION_H
