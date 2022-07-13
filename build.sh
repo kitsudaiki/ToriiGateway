2a32583e16102ac1c5f81a35c7e0b0568309a038
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-07-13 21:50:18 +0200

    Change: update dependencies

--- build.sh
+++ build.sh
@@ -101,14 +101,14 @@ get_required_kitsune_lib_repo "libKitsunemimiSakuraLang" "v0.12.0" 1
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "v0.2.0" "2ue6RNxkCDs2A7qp1xtN" 8
+get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "master" "2ue6RNxkCDs2A7qp1xtN" 8
 get_required_private_repo_gitlab "libKitsunemimiHanamiEndpoints" "v0.1.0" "ysR35grcGsLpFQiXXf1A" 1
-get_required_private_repo_gitlab "libKitsunemimiHanamiMessaging" "v0.4.1" "vkEae-QF8jvt9W2xz3LR" 8
+get_required_private_repo_gitlab "libKitsunemimiHanamiMessaging" "master" "vkEae-QF8jvt9W2xz3LR" 8
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_gitlab "libAzukiHeart" "v0.2.0" "glpat-R3vZd81MXGVUTSxgHz1q" 8
-get_required_private_repo_gitlab "libSagiriArchive" "v0.2.0" "glpat-dCkDLv96JmBscfZey4zJ" 8
+get_required_private_repo_gitlab "libAzukiHeart" "master" "glpat-R3vZd81MXGVUTSxgHz1q" 8
+get_required_private_repo_gitlab "libSagiriArchive" "master" "glpat-dCkDLv96JmBscfZey4zJ" 8
 echo ""
 echo "###########################################################################################################"
 
